21d8e14901547f66058aa72795f041b29e7e14b6
Yen-Chun,Lin <asd900606@gmail.com>
2024-09-12 18:01:08 +0800

    feat: pomodoro skeleton

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const LandingPage: React.FC = () => {
-  return <h1>LandingPage</h1>;
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-96 h-96 border-4 border-black rounded-full flex flex-col justify-center items-center">
+          <p className="text-[96px]">25:00</p>
+          <button className="bg-black text-white px-4 py-2 mt-4 block rounded">
+            Start
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default LandingPage;
