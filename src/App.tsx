d3d06f2fda34c3a062878b14e8745137af5877ff
Yen-Chun,Lin <asd900606@gmail.com>
2024-09-12 17:37:06 +0800

    feat: build react three environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-function App() {
+// App.tsx
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Box } from "@react-three/drei";
+
+const App: React.FC = () => {
   return (
-    <>
-      <h1>Hello App</h1>
-    </>
+    <div style={{ height: "100vh" }}>
+      <Canvas>
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Box position={[0, 0, 0]}>
+          <meshStandardMaterial attach="material" color="orange" />
+        </Box>
+      </Canvas>
+    </div>
   );
-}
+};
 
 export default App;
