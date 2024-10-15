56913b03e774496af3f6b209762ad1c61f661970
Yen-Chun,Lin <asd900606@gmail.com>
2024-10-15 14:05:36 +0800

    fix: can click icon in theme switch

--- src/components/Header/ThemeSwitcher.tsx
+++ src/components/Header/ThemeSwitcher.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Switch } from "@/components/ui/switch";
 import useSettingStore from "../../store/settingStore";
 import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const ThemeSwitcher = () => {
   const { setThemeMode, saveUserSettings } = useSettingStore();
@@ -28,17 +29,19 @@ const ThemeSwitcher = () => {
   }, []);
 
   return (
-    <div className="flex items-center space-x-2">
+    <div className="flex items-center gap-2 ">
+      <Button variant="link" size="header" onClick={toggleTheme}>
+        {isDarkMode ? (
+          <Moon className="w-6 h-6 text-gray-50" />
+        ) : (
+          <Sun className="w-6 h-6 text-black" />
+        )}
+      </Button>
       <Switch
         checked={isDarkMode}
         onCheckedChange={toggleTheme}
         className="bg-gray-200 dark:bg-gray-800"
       />
-      {isDarkMode ? (
-        <Moon className="w-6 h-6 text-gray-50" />
-      ) : (
-        <Sun className="w-6 h-6 text-black" />
-      )}
     </div>
   );
 };
