9864a34e2ca9cdad93589dadee3a9f360b8941de
Yen-Chun,Lin <asd900606@gmail.com>
2024-11-01 05:58:45 +0800

    refactor: add TimerInstruction.tsx

--- src/pages/TimerPage/index.tsx
+++ src/pages/TimerPage/index.tsx
@@ -8,6 +8,7 @@ import {
   ChevronsDown,
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import TimerInstruction from "./TimerInstruction";
 
 interface TimerPageProps {
   page: string | null;
@@ -45,34 +46,7 @@ const TimerPage: React.FC<TimerPageProps> = ({
     <>
       <div className="z-30 ">
         {showInstructions && (
-          <div className="fixed inset-0 flex justify-center items-center bg-gray-800 bg-opacity-50 z-40">
-            <div className="bg-white p-5 rounded shadow-lg">
-              <h2 className="text-xl mb-4">操作說明</h2>
-              <p className="mb-4">
-                歡迎使用計時器和Todo List！以下是一些操作說明：
-              </p>
-              <ul className="list-disc ml-5 mb-4">
-                <li>您可以設定計時器的分鐘數，預設為 25 分鐘。</li>
-                <li>點擊顯示的時間可以進入編輯模式，調整計時分鐘數。</li>
-                <li>使用「+」和「-」按鈕可快速增加或減少 5 分鐘。</li>
-                <li>點擊「開始」以啟動計時器，計時器將開始倒數。</li>
-                <li>計時期間，您無法修改分鐘數。</li>
-                <li>計時完成時，將會顯示瀏覽器通知</li>
-                <li>您可以在完成後重置計時器或繼續添加新任務。</li>
-              </ul>
-              <p className="mb-4">
-                請記得在計時器運行時，不要忘記完成您的Todos哦！
-              </p>
-              <div className="flex justify-center">
-                <button
-                  className="bg-blue-500 text-white p-2 rounded"
-                  onClick={handleCloseInstructions}
-                >
-                  不再顯示
-                </button>
-              </div>
-            </div>
-          </div>
+          <TimerInstruction handleCloseInstructions={handleCloseInstructions} />
         )}
         <div className="w-screen h-screen flex justify-center items-center relative">
           <Timer
