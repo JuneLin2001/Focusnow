6df532b20486ced2e2d1292d6d568d07fd8f9197
Yen-Chun,Lin <asd900606@gmail.com>
2024-11-06 10:56:32 +0800

    feat: add Tooltip and set style

--- src/components/Analytics/CompletedTodos.tsx
+++ src/components/Analytics/CompletedTodos.tsx
@@ -2,6 +2,12 @@ import React from "react";
 import { UserAnalytics } from "../../types/type";
 import dayjs from "dayjs";
 import { Card } from "@/components/ui/card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 interface CompletedTodosProps {
@@ -26,7 +32,7 @@ const CompletedTodos: React.FC<CompletedTodosProps> = ({
   const hasData = completedTodosCount > 0;
 
   return (
-    <div className="flex h-full flex-col">
+    <div className="flex size-full flex-col">
       <div className="justify-between text-center">
         {hasData && (
           <h2 className="mt-4 text-lg font-semibold">
@@ -36,52 +42,51 @@ const CompletedTodos: React.FC<CompletedTodosProps> = ({
       </div>
       <ScrollArea className="h-full">
         {hasData ? (
-          <div className="flex h-[calc(90vh-12rem)] flex-col p-4">
-            {filteredAnalytics.map((analytics, index) => {
-              if (analytics.todos.length > 0) {
-                return (
-                  <Card key={index} className="mb-2 flex w-full p-4">
-                    <div className="space-y-2">
-                      {analytics.todos.map((todo) => {
-                        const doneTime = todo.doneTime;
-                        const doneTimeDate =
-                          doneTime &&
-                          doneTime.seconds !== undefined &&
-                          doneTime.nanoseconds !== undefined
-                            ? convertFirestoreTimestampToDate(
-                                doneTime.seconds,
-                                doneTime.nanoseconds,
-                              )
-                            : null;
+          <div className="flex h-[calc(90vh-12rem)] max-w-full flex-col p-4">
+            {filteredAnalytics.map((analytics, index) =>
+              analytics.todos.length > 0 ? (
+                <Card key={index} className="mb-2 flex w-full max-w-full p-4">
+                  <div className="space-y-2">
+                    {analytics.todos.map((todo) => {
+                      const doneTimeDate = todo.doneTime?.seconds
+                        ? convertFirestoreTimestampToDate(
+                            todo.doneTime.seconds,
+                            todo.doneTime.nanoseconds,
+                          )
+                        : null;
 
-                        return (
-                          <div
-                            key={todo.id}
-                            className="border-b border-gray-300 py-2 last:border-b-0 dark:border-gray-600"
-                          >
-                            {/* TODO:不要使用w-40，用相對的寬度 */}
-                            <h3 className="w-full min-w-40 max-w-40 truncate font-semibold text-gray-800 dark:text-white">
-                              {todo.title}
-                            </h3>
-                            <p className="text-sm text-gray-600 dark:text-gray-400">
-                              {`完成時間：${
-                                doneTimeDate
-                                  ? dayjs(doneTimeDate).format("MM-DD HH:mm")
-                                  : "未完成"
-                              }`}
-                            </p>
-                          </div>
-                        );
-                      })}
-                    </div>
-                  </Card>
-                );
-              }
-              return null;
-            })}
+                      return (
+                        <div
+                          key={todo.id}
+                          className="border-b border-gray-300 py-2 last:border-b-0 dark:border-gray-600"
+                        >
+                          <TooltipProvider>
+                            <Tooltip>
+                              <TooltipTrigger asChild>
+                                <p className="w-full max-w-44 truncate text-ellipsis font-semibold text-gray-800 dark:text-white">
+                                  {todo.title}
+                                </p>
+                              </TooltipTrigger>
+                              <TooltipContent>{todo.title}</TooltipContent>
+                            </Tooltip>
+                          </TooltipProvider>
+                          <p className="text-sm text-gray-600 dark:text-gray-400">
+                            {`完成時間：${
+                              doneTimeDate
+                                ? dayjs(doneTimeDate).format("MM-DD HH:mm")
+                                : "未完成"
+                            }`}
+                          </p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </Card>
+              ) : null,
+            )}
           </div>
         ) : (
-          <div className="flex items-center justify-center">
+          <div className="flex size-full flex-col justify-center text-center">
             <p className="text-gray-500 dark:text-gray-200">沒有完成的 Todos</p>
           </div>
         )}
