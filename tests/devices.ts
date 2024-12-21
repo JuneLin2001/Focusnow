10ab1cc18f9f4f72fd115469e4d684113ea35652
Yen-Chun,Lin <asd900606@gmail.com>
2024-12-21 21:59:37 +0800

    refactor: separate files

---
+++ tests/devices.ts
@@ -0,0 +1,14 @@
+export const devices = [
+  {
+    name: "Desktop",
+    viewport: { width: 1920, height: 1080 },
+    isMobile: false,
+    deviceScaleFactor: 1,
+  },
+  {
+    name: "Mobile",
+    viewport: { width: 360, height: 640 },
+    isMobile: true,
+    deviceScaleFactor: 2,
+  },
+];
--- tests/structureTest.spec.ts
+++ tests/structureTest.spec.ts
@@ -1,36 +1,48 @@
 import { test, expect } from "@playwright/test";
+import { devices } from "./devices";
 
-test("has title", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
+devices.forEach((device) => {
+  test.describe(`${device.name} tests`, () => {
+    test.use({
+      viewport: device.viewport,
+      deviceScaleFactor: device.deviceScaleFactor,
+      isMobile: device.isMobile,
+    });
 
-  await expect(page).toHaveTitle(/Focusnow/);
-});
+    test("Can navigate to the home page", async ({ page }) => {
+      await page.goto("http://localhost:5173/");
+      await expect(page).toHaveTitle(/Focusnow/);
+    });
+    if (device.isMobile) {
+      test("RWD test", async ({ page }) => {
+        await page.goto("http://localhost:5173/");
+        const navMenu = page.locator(
+          '//*[@id="root"]/header/div/div[1]/button[1]',
+        );
+        await expect(navMenu).toBeVisible();
+      });
+    }
 
-test("RWD test", async ({ browser }) => {
-  const context = await browser.newContext({
-    viewport: { width: 360, height: 640 },
-    deviceScaleFactor: 2,
-    isMobile: true,
+    test("Login test", async ({ page }) => {
+      await page.goto("http://localhost:5173/");
+      await page.locator(".absolute > .inline-flex").click();
+      await page
+        .getByRole("banner")
+        .getByRole("button")
+        .nth(device.isMobile ? 3 : 4)
+        .click();
+
+      const loginButton = page.getByRole("heading", { name: "登入" });
+      await expect(loginButton).toBeVisible();
+
+      await page.getByRole("button", { name: "logo 使用訪客帳號登入" }).click();
+      await page.getByRole("button", { name: "訪客帳號" }).click();
+
+      const logoutButton = page
+        .getByRole("banner")
+        .getByRole("button")
+        .nth(device.isMobile ? 3 : 4);
+      await expect(logoutButton).toBeVisible();
+    });
   });
-  const page = await context.newPage();
-
-  await page.goto("http://localhost:5173/");
-
-  const navMenu = page.locator('//*[@id="root"]/header/div/div[1]/button[1]');
-  await expect(navMenu).toBeVisible();
-});
-
-test("Login test", async ({ page }) => {
-  await page.goto("http://localhost:5173/");
-  await page.locator(".absolute > .inline-flex").click();
-  await page.getByRole("banner").getByRole("button").nth(4).click();
-
-  const loginButton = page.getByRole("heading", { name: "登入" });
-  await expect(loginButton).toBeVisible();
-
-  await page.getByRole("button", { name: "logo 使用訪客帳號登入" }).click();
-  await page.getByRole("button", { name: "訪客帳號" }).click();
-
-  const logoutButton = page.getByRole("banner").getByRole("button").nth(4);
-  await expect(logoutButton).toBeVisible();
 });
