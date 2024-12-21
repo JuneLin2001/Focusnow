7523d784549e9e83fbb715fd36ebafde4fdda056
Yen-Chun,Lin <asd900606@gmail.com>
2024-12-21 23:47:08 +0800

    feat: add todo list test

--- tests/Timer.spec.ts
+++ tests/Timer.spec.ts
@@ -4,10 +4,7 @@ import { devices } from "./devices";
 async function navigateToTimerPage(page, device) {
   await page.goto("http://localhost:5173/");
   page.locator(".absolute > .inline-flex").click();
-  if (device.isMobile) {
-    const navMenu = page.locator('//*[@id="root"]/header/div/div[1]/button[1]');
-    await navMenu.click();
-  }
+
   const timerButton = device.isMobile
     ? page.getByRole("button", { name: "Timer" })
     : page.getByRole("navigation").getByRole("button", { name: "Timer" });
@@ -23,7 +20,9 @@ devices.forEach((device) => {
     });
 
     test("Timer Instructions", async ({ page }) => {
+      test.setTimeout(120000);
       await navigateToTimerPage(page, device);
+      await page.waitForTimeout(3000);
 
       const TimerInstructions = page.getByText(
         "點擊這裡以控制 Todo List 的開啟與關閉。",
@@ -37,6 +36,40 @@ devices.forEach((device) => {
       await NextStep.click();
       await expect(PreviousStep && NextStep && SkipStep).toBeEnabled();
       await SkipStep.click();
+
+      await expect(TimerInstructions).not.toBeVisible();
+    });
+
+    test("Todo List", async ({ page }) => {
+      test.setTimeout(120000);
+      await navigateToTimerPage(page, device);
+      await page.waitForTimeout(3000);
+      await page.locator('[data-test-id="button-skip"]').click();
+
+      const TodoInput = page.locator('input[placeholder="New Todo"]');
+      await TodoInput.fill("123ABC一二三");
+
+      const TodoAddButton = page.locator("#new-todo").getByRole("button");
+      await TodoAddButton.click();
+
+      const TodoBody = page.locator('//*[@id="todo-list"]/ul/li/input[2]');
+      await TodoBody.waitFor({ state: "visible" });
+      await expect(TodoBody).toHaveValue("123ABC一二三");
+
+      await TodoBody.click();
+      await TodoBody.fill("編輯測試edit test");
+      await expect(TodoBody).toHaveValue("編輯測試edit test");
+
+      const TodoCheckbox = page.locator('input[type="checkbox"]');
+      await TodoCheckbox.click();
+
+      const TodoItem = page.getByRole("listitem").getByRole("textbox");
+      await expect(TodoItem).toHaveClass(/text-gray-500/);
+      await expect(TodoItem).toHaveClass(/line-through/);
+
+      const TodoDeleteButton = page.locator("li").getByRole("button");
+      await TodoDeleteButton.click();
+      await expect(TodoBody).not.toBeVisible();
     });
   });
 });
