d85afd243301bef86596325d49320ba101d6aef9
Yen-Chun,Lin <asd900606@gmail.com>
2025-09-30 01:19:08 +0800

    refactor: remove page name store

--- app/components/Initializer/TimerPageInitializer.tsx
+++ app/components/Initializer/TimerPageInitializer.tsx
@@ -1,18 +1,15 @@
 "use client";
 
 import useSceneStore from "@/store/useSceneStore";
-import usePageStore from "@/store/usePageStore";
 import { useEffect } from "react";
 
 const TimerPageInitializer = () => {
   const { setTargetPosition, setLookAtPosition } = useSceneStore();
-  const { setPage } = usePageStore();
 
   useEffect(() => {
-    setPage("timer");
     setTargetPosition([-50, 12, -150]);
     setLookAtPosition([0, 0, 0]);
-  }, [setLookAtPosition, setPage, setTargetPosition]);
+  }, [setLookAtPosition, setTargetPosition]);
 
   return null;
 };
